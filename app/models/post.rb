4f2ca69eece126260c0be8a4db4a0314781da341
nolan-dyke <nolan.g.dyke@gmail.com>
2020-05-12 10:37:02 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ActiveRecord::Base
+    # include ActiveModel::Validations
+    # validates_with TitleValidator
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+
+    def clickbait
+        check = false
+        if title.present?
+            if title.include?("Secret") 
+                check = true 
+            elsif title.include?("Top")
+                check = true 
+            elsif title.include?("Won't Believe")
+                check = true 
+            elsif title.include?("Guess")
+                check = true 
+            end 
+        end 
+
+        if check == false 
+            errors.add(:title, "not click bait")
+        end 
+    end 
 end
